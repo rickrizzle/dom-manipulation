5ebecfb68c8001b94530a5f0b9fb93658ab66e8c
rickrizzle <rickh39@gmail.com>
2019-06-20 22:24:11 -0400

    Add dom manipulation

---
+++ main.js
@@ -0,0 +1,32 @@
+console.log("Hello World");
+
+let displayPromptButton = document.getElementById("displayPromptButton");
+
+/*
+//DOMContent Loaded listens for when DOM conent is loaded
+document.addEventListener("DOMContentLoaded", function() {
+  let displayPromptButton = document.getElementById("displayPromptButton");
+
+  //This code (which was previously by itself) doesn't execute until above DOM
+  //contentloaded event has fired.
+  displayPromptButton.addEventListener("click", function() {
+    console.log("clicked");
+  });
+});
+*/
+
+function addListItem(input, target) {
+  let listItem = document.createElement("li");
+  listItem.innerHTML = input;
+  target.appendChild(listItem);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  let displayPromptButton = document.getElementById("displayPromptButton");
+  let view = document.getElementById("pokemon");
+
+  displayPromptButton.addEventListener("click", function() {
+    let input = prompt("What is your favorite Pokemon?");
+    addListItem(input, view);
+  });
+});
